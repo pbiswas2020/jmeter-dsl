0998af3a6c36c3b93a2c708b76a44b23ba7e2979
Paramita Biswas <paramitadatbis@gmail.com>
2025-09-25 18:55:27 -0400

    Dev branch + jmx

--- src/test/java/org/home/qa/PerformanceTest.java
+++ src/test/java/org/home/qa/PerformanceTest.java
@@ -8,7 +8,8 @@ import static us.abstracta.jmeter.javadsl.dashboard.DashboardVisualizer.*;
 
 import java.io.IOException;
 import java.time.Duration;
-
+import java.util.Map;
+import org.apache.http.entity.ContentType;
 import org.junit.jupiter.api.Test;
 
 import us.abstracta.jmeter.javadsl.core.TestPlanStats;
@@ -19,18 +20,28 @@ public class PerformanceTest {
 	public void testPerformance() throws IOException {
 		TestPlanStats stats = 
 				testPlan(
+						// CSV Data Set Config
+				        csvDataSet("src/test/resources/users.csv")  // file in resources or path
+				            .variableNames("username", "password")
+				            .ignoreFirstLine(true),
+				            
+//						threadGroup(2, 10, 
+//								httpSampler("home","https://the-internet.herokuapp.com/")
+//						),
+//						threadGroup(2, 10,		
+//								httpSampler("login","https://the-internet.herokuapp.com/login")
+//						),
+//						threadGroup(2, 10,		
+//								httpSampler("forgotPassword","https://the-internet.herokuapp.com/forgot_password")
+//						),
 						threadGroup(2, 10, 
-								httpSampler("home","https://the-internet.herokuapp.com/")
-						),
-						threadGroup(2, 10,		
-								httpSampler("login","https://the-internet.herokuapp.com/login")
-						),
-						threadGroup(2, 10,		
-								httpSampler("forgotPassword","https://the-internet.herokuapp.com/forgot_password")
+								httpSampler("home","https://the-internet.herokuapp.com/authenticate")
+								.post("{\"${username}\": \"${password}\"", ContentType.APPLICATION_JSON)
+
 						)
-						, dashboardVisualizer(),
+						, dashboardVisualizer()
 				// this is just to log details of each request stats
-						jtlWriter("target/jtls")
+						,jtlWriter("target/jtls")
 				).run();
 		assertThat(stats.overall().sampleTimePercentile99()).isLessThan(Duration.ofSeconds(5));
 	}
